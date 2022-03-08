65afcf8328474cf01607d337425f5f5c12499662
username <useremail>
2022-03-08 17:22:23 +0900

    2022-03-08 부트스트랩 활용

---
+++ shop/src/App.js
@@ -0,0 +1,43 @@
+
+import logo from './logo.svg';
+import { Navbar,Nav,NavDropdown,Container,Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">GardenShop</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+<div className='Jumbotron'>
+  <h1>30% Season off</h1>
+  <p>This is a simple hero unit, a simple
+    jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+  <Button variant="primary">Primary</Button>       
+  </p>
+  </div>
+
+</div>
+  );
+}
+
+export default App;
