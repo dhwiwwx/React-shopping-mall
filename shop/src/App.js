d4ea3f822b2163713afc968541b67fd086a77f04
dhwiwwx <sdh210320@sdh.hs.kr>
2022-03-14 19:52:41 +0900

    2022-03-14 detail page 이름 데이터 바인딩

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,9 @@
-import { Navbar,Nav,NavDropdown,Container,Button} from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container, Button } from 'react-bootstrap';
 import './App.css';
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 import Data from './data.js';
 import { Modal } from 'bootstrap';
-import {Link, Route, Switch} from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import Detail from './Detail';
 
 function App() {
@@ -11,70 +11,70 @@ function App() {
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">GardenShop</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link><Link to="/">Home</Link></Nav.Link>
-        <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+        <Container>
+          <Navbar.Brand href="#home">GardenShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link><Link to="/">Home</Link></Nav.Link>
+              <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-  <Switch>
-  <Route exact path="/">
-  <div className='Jumbotron'>
-  <h1>30% Season off</h1>
-  <p>This is a simple hero unit, a simple
-    jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-  <Button variant="primary">Primary</Button>       
-  </p>
-  </div>
+      <Switch>
+        <Route exact path="/">
+          <div className='Jumbotron'>
+            <h1>30% Season off</h1>
+            <p>This is a simple hero unit, a simple
+              jumbotron-style component for calling
+              extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Primary</Button>
+            </p>
+          </div>
 
-  <div className="container">
-      <div className='row'>
-        {
-          shoes.map((a, i)=>{
-            return<Card shoes={shoes[i]} i={i} key={i}/>
-          })
-        }
-      </div>
+          <div className="container">
+            <div className='row'>
+              {
+                shoes.map((a, i) => {
+                  return <Card shoes={shoes[i]} i={i} key={i} />
+                })
+              }
+            </div>
+          </div>
+        </Route>
+
+
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나 적었을떄 이거 보여주셈</div>
+        </Route>
+
+      </Switch>
     </div>
-    </Route>
-  
-
-  <Route path="/detail/:id">
-    <Detail/>
-    </Route>
-
-  <Route path="/:id">
-    <div>아무거나 적었을떄 이거 보여주셈</div>
-    </Route>
-
-    </Switch>
-  </div>
   );
 }
 
-function Card(props){
+function Card(props) {
   return (
-      <div className="col-md-4">
-            <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) +'.jpg' } width="100%" />
-            <h4>{ props.shoes.title }</h4>
-            <p>{ props.shoes.content } & { props.shoes.price }</p>
-      </div>
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="100%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content} & {props.shoes.price}</p>
+    </div>
   )
 }
 
--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,23 +1,26 @@
-import React,{useState} from "react";
-import { useHistory} from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
 
-function Detail(){
-    let history = useHistory();
-    return(
-        <div className="container">
+function Detail(props) {
+
+  let { id } = useParams();
+  let history = useHistory();
+
+  return (
+    <div className="container">
       <div className="row">
         <div className="col-md-6">
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
-          <button className="btn btn-danger">주문하기</button> 
-          <button className="btn btn-danger" onClick={()=>{history.push('/');}}>뒤로가기</button> 
+          <h4 className="pt-5">{props.shoes[id].title}</h4>
+          <p>{props.shoes[id].content}</p>
+          <p>{props.shoes[id].price}</p>
+          <button className="btn btn-danger">주문하기</button>
+          <button className="btn btn-danger" onClick={() => { history.push('/'); }}>뒤로가기</button>
         </div>
       </div>
-</div> 
-    )
+    </div>
+  )
 }
 export default Detail;
