518d2285f3d0b63efed0975f276abc1b2676facc
dhwiwwx <sdh210320@sdh.hs.kr>
2022-08-02 16:04:08 +0900

    useParams 활용 상세페이지 수정

--- shop/src/App.js
+++ shop/src/App.js
@@ -52,7 +52,10 @@ function App() {
             </div>
           </>
         } />
-        <Route path="/detail" element={<Detail />} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+
+
+
         <Route path="*" element={<div>없는페이지</div>} />
         <Route path='/about' element={<About></About>}>
           <Route path='member' element={<div>member</div>} />
